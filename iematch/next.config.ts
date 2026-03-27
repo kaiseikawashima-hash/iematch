3a45ad41b282584d9f545a22e8e94e73faf3960c
kaiseikawashima-hash <kaisei.kawashima03@gmail.com>
2026-03-27 14:29:23 +0900

    next.config.tsにドメイン追加・画像管理改善

--- iematch/next.config.ts
+++ iematch/next.config.ts
@@ -7,6 +7,30 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "cdn.shopify.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.shopifycdn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.pixabay.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.imgur.com",
+      },
     ],
   },
 };
